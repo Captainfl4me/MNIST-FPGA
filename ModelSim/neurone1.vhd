00c54a668dea8a38d05252c091e2f86b692a4b58
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-02-03 13:25:55 +0100

    fix: coef implementation

--- ModelSim/neurone1.vhd
+++ ModelSim/neurone1.vhd
@@ -24,17 +24,20 @@ end entity;
 
 architecture a1 of neurone1 is
     -- constant nb_Neurones : integer := 100;
-    signal mult1, mult1_L : typtabcst(0 to lngimag-1);  -- Signaux Multiplieurs
-	signal mult2, mult2_L   : typtabcst(0 to nbsymbol-1);
+    signal mult1 : typtabcst(0 to nbneuron-1);  -- Signaux Multiplieurs
+	signal mult2 : typtabcst(0 to nbsymbol-1);
     
-    signal add1, add1_L : typtabaccu; -- Signaux Additionneurs
-    signal add2, add2_L : typtabaccu2;  -- Signaux Registre Additionneurs
+    signal add1 : typtabaccu; -- Signaux Additionneurs
+    signal add2 : typtabaccu2;  -- Signaux Registre Additionneurs
 
-    signal activf1, activf1_L : typtabaccu; -- Signaux fonctions d'activation
-    signal activf2, activf2_L : typtabaccu2; -- Signaux Registre fonctions d'activation
+    signal activf1 : typtabaccu; -- Signaux fonctions d'activation
+    signal activf2 : typtabaccu2; -- Signaux Registre fonctions d'activation
 
-	signal pixel_index     : integer range 0 to lngimag-1 := 0;
-	signal image_finish    : std_logic := '0';
+	type state is (sm_reset, sm_compute, sm_wait_start);
+	signal cur_state : state := sm_reset;
+
+	signal pixel_index     : integer range 0 to lngimag-1  := 0;
+	signal neuron_index    : integer range 0 to nbneuron-1 := 0;
 	signal clkimg2, start2 : std_logic := '0';
 	signal pixelin         : sfixed(0 downto -nbitq) := (others => '0');
 begin
@@ -50,94 +53,91 @@ begin
 		end if;
 	end process;
 
+    -------------------------- State detection ---------------------------------
+	process (clock, reset) is
+	begin
+		if reset = '0' then
+			cur_state <= sm_reset;
+		elsif rising_edge(clock) then
+			case cur_state is
+				when sm_reset => cur_state <= sm_compute;
+				when sm_compute => 
+					if start = '1' and start2 = '0' then -- rising edge
+						cur_state <= sm_reset;
+					elsif  pixel_index >= lngimag-1 then 
+						cur_state <= sm_wait_start; 
+					end if;
+				when sm_wait_start =>
+					if start = '1' and start2 = '0' then -- rising edge
+						cur_state <= sm_reset;
+					end if;
+			end case;
+		end if;
+	end process;
+
     -------------------------- Resynchro de image sur clock & Compteur index pixel ---------------------------------
 	process (clock, reset) is
 	begin
 		if reset = '0' then
 			pixelin <= (others => '0');
 			pixel_index <= 0;
-			image_finish <= '0';
 		elsif rising_edge(clock) then
 			if clkimg = '1' and clkimg2 = '0' then -- rising edge
 				pixelin <= sfixed('0' & image);
 
-				if start = '1' and start2 = '0' then -- rising edge
-					pixel_index <= 0;
-					image_finish <= '0';
+				if cur_state = sm_compute then
+					pixel_index <= pixel_index + 1;
 				else
-					if pixel_index >= lngimag-1  then
-						pixel_index <= 0;
-						image_finish <= '1';
-					elsif image_finish = '0' then
-						pixel_index <= pixel_index + 1;
-					else
-						pixel_index <= pixel_index;
-					end if;
+					pixel_index <= 0;
 				end if;
 			end if;
 		end if;
 	end process;
 
-    -------------------------- Etages de registres ---------------------------------
-    process(clock)
-    begin
-        if rising_edge(clock) then
-            mult1_L <= mult1;
-            mult2_L <= mult2;
-
-            add1_L <= add1;
-            add2_L <= add2;
-
-            activf1_L <= activf1;
-            activf2_L <= activf2;
-        end if;
-    end process;
-
     -------------------------- Calculs neurones couche 1 ---------------------------------
 
-
     gen_Mult_1e : for i in 0 to (mult1'length-1) generate
-        mult1(i) <= resize(allcoef1(i) * pixelin * to_sfixed(ccf, 5, 0), mult1(i), fixed_wrap, fixed_truncate);
+        mult1(i) <= resize(coef1(pixel_index, i) * pixelin * to_sfixed(ccf, 5, 0), mult1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     gen_Add_1e : for i in 0 to (add1'length-1) generate
-        add1(i) <= resize(mult1_L(i) + to_sfixed(cct, 6, 0), add1(i), fixed_wrap, fixed_truncate);
+        add1(i) <= resize(add1(i) + mult1(i) + to_sfixed(cct, 6, 0), add1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     -------------------------- Fonction d'activation 1 ---------------------------------
 
-    gen_FctActiv_1e : for i in 0 to (add1_L'length-1) generate
-		activf1(i) <= to_sfixed(1, activf1(i)) when add1_L(i) > 2 else
-					  to_sfixed(-1, activf1(i)) when add1_L(i) < -2 else
-					  resize(shift_right(add1_L(i), 1), activf1(i), fixed_wrap, fixed_truncate);
+    gen_FctActiv_1e : for i in 0 to (add1'length-1) generate
+		activf1(i) <= to_sfixed(1, activf1(i)) when add1(i) > 2 else
+					  to_sfixed(-1, activf1(i)) when add1(i) < -2 else
+					  resize(shift_right(add1(i), 1), activf1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     -------------------------- Calculs neurones couche 2 ---------------------------------
 
     gen_Mult_2e : for i in 0 to (mult2'length-1) generate
-        mult2(i) <= resize(allcoef2(i) * activf1_L(i) * to_sfixed(ccf2, 5, 0), mult2(i), fixed_wrap, fixed_truncate);
+        mult2(i) <= resize(coef2(neuron_index, i) * activf1(i) * to_sfixed(ccf2, 5, 0), mult2(i), fixed_wrap, fixed_truncate);
     end generate;
 
     gen_Add_2e : for i in 0 to (add2'length-1) generate
-        add2(i) <= resize(mult2_L(i) + to_sfixed(cct2, 6, 0), add2(i), fixed_wrap, fixed_truncate);
+        add2(i) <= resize(add2(i) + mult2(i) + to_sfixed(cct2, 6, 0), add2(i), fixed_wrap, fixed_truncate);
     end generate;
 
 
     -------------------------- Fonction d'activation 2 ---------------------------------
 
-    process_2e_Activation : process(add2_L)
+    process_2e_Activation : process(add2)
         variable maxT, maxT2 : sfixed(5 downto -nbitq) := to_sfixed(0, 5, -nbitq);  -- Les 2 maximums du réseau
         variable maxI, maxI2 : integer range 0 to nbsymbol := 0;	                              -- Indices des neurones maximum
     begin
-        for i in 0 to (add2_L'length-1) loop
-            if add2_L(i) > maxT then
+        for i in 0 to (add2'length-1) loop
+            if add2(i) > maxT then
                 maxT2 := maxT;
                 maxI2 := maxI;
                 maxI  := i;
-                maxT  := resize(add2_L(i), maxT, fixed_wrap, fixed_truncate); 
-            elsif add2_L(i) > maxT2 then
+                maxT  := resize(add2(i), maxT, fixed_wrap, fixed_truncate); 
+            elsif add2(i) > maxT2 then
                 maxI2 := i;
-                maxT2 := resize(add2_L(i), maxT2, fixed_wrap, fixed_truncate); 
+                maxT2 := resize(add2(i), maxT2, fixed_wrap, fixed_truncate); 
             end if;
         end loop;
 
