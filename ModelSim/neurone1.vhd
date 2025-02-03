5000bca50ca8e5301ceb5ae4d51f46ef9f62bf7c
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-02-03 09:33:38 +0100

    fix: neurone merge bugs

--- ModelSim/neurone1.vhd
+++ ModelSim/neurone1.vhd
@@ -24,8 +24,8 @@ end entity;
 
 architecture a1 of neurone1 is
     -- constant nb_Neurones : integer := 100;
-    signal mult1  , mult2,  : typtabcst;  -- Signaux Multiplieurs
-    signal mult1_L, mult2_L : typtabcst;  -- Signaux Registre Multiplieurs
+    signal mult1  , mult2   : typtabcst(0 to lngimag-1);  -- Signaux Multiplieurs
+    signal mult1_L, mult2_L : typtabcst(0 to lngimag-1);  -- Signaux Registre Multiplieurs
     
     signal add1  , add2     : typtabaccu; -- Signaux Additionneurs
     signal add1_L, add2_L   : typtabaccu;  -- Signaux Registre Additionneurs
@@ -34,7 +34,7 @@ architecture a1 of neurone1 is
     signal activf1_L, activf2_L : typtabaccu; -- Signaux Registre fonctions d'activation
 
 	signal clkimg2 : std_logic := '0';
-	signal pixelin : unsigned(7 downto 0) := (others => '0');
+	signal pixelin : sfixed(0 downto -nbitq) := (others => '0');
 begin
 	ready <= '1';
 	process (clock, reset) is
@@ -53,15 +53,15 @@ begin
 			pixelin <= (others => '0');
 		elsif rising_edge(clock) then
 			if clkimg = '1' and clkimg2 = '0' then -- rising edge
-				pixelin <= image;
+				pixelin <= sfixed('0' & image);
 			end if;
 		end if;
 	end process;
 
     -------------------------- Etages de registres ---------------------------------
-    process(clk)
+    process(clock)
     begin
-        if rising_edge(clk) then
+        if rising_edge(clock) then
             mult1_L <= mult1;
             mult2_L <= mult2;
 
@@ -76,45 +76,34 @@ begin
     -------------------------- Calculs neurones couche 1 ---------------------------------
 
     gen_Mult_1e : for i in 0 to (allcoef1'length-1) generate
-        mult1(i) <= sfixed(allcoef1(i) * image * ccf, mult1(i), fixed_wrap, fixed_truncate);
+        mult1(i) <= resize(allcoef1(i) * pixelin * to_sfixed(ccf, 5, 0), mult1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     gen_Add_1e : for i in 0 to (allcoef1'length-1) generate
-        add1(i) <= sfixed( mult1_L(i) + cct, add1(i), fixed_wrap, fixed_truncate);
+        add1(i) <= resize( mult1_L(i) + to_sfixed(cct, 6, 0), add1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     -------------------------- Fonction d'activation 1 ---------------------------------
 
     gen_FctActiv_1e : for i in 0 to (allcoef1'length-1) generate
-        if add1_L(i) > 2 then
-            activf1(i) <= sfixed(1, activf1(i), fixed_wrap, fixed_truncate);
-        elsif add1_L(i) < -2 then
-            activf1(i) <= sfixed(-1, activf1(i), fixed_wrap, fixed_truncate);
-        else 
-            activf1(i) <= sfixed(add1_L/2.0, activf1(i), fixed_wrap, fixed_truncate);
-        end if;   
+		activf1(i) <= to_sfixed(1, activf1(i)) when add1_L(i) > 2 else
+					  to_sfixed(-1, activf1(i)) when add1_L(i) < -2 else
+					  resize(shift_right(add1_L(i), 1), activf1(i), fixed_wrap, fixed_truncate);
     end generate;
 
     -------------------------- Calculs neurones couche 2 ---------------------------------
 
     gen_Mult_2e : for i in 0 to (allcoef1'length-1) generate
-        mult2(i) <= sfixed(allcoef2(i) * activf1_L(i)) * ccf2, mult2(i), fixed_wrap, fixed_truncate);
+        mult2(i) <= resize(allcoef2(i) * activf1_L(i) * to_sfixed(ccf2, 5, 0), mult2(i), fixed_wrap, fixed_truncate);
     end generate;
 
     gen_Add_2e : for i in 0 to (allcoef1'length-1) generate
-        add2(i) <= sfixed( mult2_L(i) + cct2, add2(i), fixed_wrap, fixed_truncate);
+        add2(i) <= resize( mult2_L(i) + to_sfixed(cct2, 6, 0), add2(i), fixed_wrap, fixed_truncate);
     end generate;
 
 
     -------------------------- Fonction d'activation 2 ---------------------------------
 
     gen_FctActiv_2e : for i in 0 to (allcoef1'length-1) generate
-        if add2_L(i) > 2 then
-            activf2(i) <= sfixed(1, activf2(i), fixed_wrap, fixed_truncate);
-        elsif add2_L(i) < -2 then
-            activf2(i) <= sfixed(-1, activf2(i), fixed_wrap, fixed_truncate);
-        else 
-            activf2(i) <= sfixed(add2_L/2.0, activf2(i), fixed_wrap, fixed_truncate);
-        end if;   
     end generate;
 end architecture;
